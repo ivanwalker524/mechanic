05b4959e55e8d5babe0ddb7cc48ed9e0bb7efb5c
ivanwalker524 <ivanwalker524@gmail.com>
2022-10-14 09:32:53 +0300

    flex-wrap

---
+++ mechanic.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Motor Repair</title>
+    <style>
+       <?php
+            require './mec.css'
+       ?>
+    </style>
+</head>
+<body>
+    <div class="background_color">
+        
+            <!-- <div class="nav">
+                <div class="skew">
+                    <div>
+                    </div>
+                    <div><h1>CAR<span>REPAIR</span></h1></div>
+                </div>
+                <div class="call">
+                    <p>Tel:+1 959 552 5963; +1 959 552 5975</p>
+                    <p>Old Kampla road, Uganda</p>
+                </div>
+                <div class="about">
+                    <ul>
+                        <li><a href="#">HOME</a></li>
+                        <li><a href="#">ABOUT</a></li>
+                        <li><a href="#">MAINTENANCE</a></li>
+                        <li><a href="#">REPAIR</a></li>
+                        <li><a href="#">PRICE LIST</a></li>
+                        <li><a href="#">LOCATIONS</a></li>
+                    </ul>
+                </div>
+            </div> -->
+            <div class="background">
+            <div>
+            </div>
+        </div>
+        <div class="wrap">
+            <div>
+                <div>
+                    <div>
+                        <img src="./img/img-5.jpg" alt="">
+                        <div class="center bg">
+                            <p>SCHEDULE</p>
+                            <p>SERVICES</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore, possimus.</p>
+                            <p><a href="#">more</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <img src="./img/img-4.jpg" alt="">
+                        <div class="center">
+                            <p>PREVENTIVE</p>
+                            <P>MAINTENANCE</P>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente, delectus.</p>
+                            <p><a href="#">more</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <img src="./img/img-2.jpg" alt="">
+                        <div class="center">
+                            <p>REPAIR</p>
+                            <p>SERVICE</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam, obcaecati.</p>
+                            <p><a href="#">more</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="./img/img-5.jpg" alt="">
+                    <div class="center">
+                        <p>TIRE & WHEEL</p>
+                        <p>SERVICE</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, quaerat.</p>
+                        <p><a href="#">more</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="double">
+            <div class="first">
+                <div>
+                    <p>OUR <span>SERVICES</span></p>
+                    <div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, beatae?</p>
+                        </div>
+                        <div>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, est?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="second">
+                <div>
+                    <p>REVIEW</p>
+                    <div>
+                        <div>
+                            <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, suscipit.</P>
+                        </div>
+                        <p class="icon">
+                            <span><?php include './svg/user.svg' ?></span>
+                        </p>
+                        <div>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, debitis!</p>
+                        </div>
+                        <p></p>
+                        <p><a href="#">See all</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            
+        </footer>
+    </div>
+</body>
+</html>
